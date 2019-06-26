05f79a25c463f8c25cbd1fda772562f63efed346
Changrui <scrdyx@163.com>
2019-06-26 10:42:16 +0800

    enhance build.sh and modify mode of some files

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/sh
-mkdir build
-cd build && cmake ..
-make
+
+# create build directory
+if [ -e build -a -d build ]
+then
+    cd build
+else
+    mkdir build && cd build
+fi
+
+# compile source files and build executables
+cmake .. && cmake --build ./
+
+# create binary library
+if ( test $? -eq 0 ); then
+    cd ..
+    if [ -e bin -a -d bin ]; then
+        cd bin && rm -fr *
+    else
+       mkdir bin && cd bin
+    fi
+    cp ../build/ch2/src/cs-* ./
+fi
+
